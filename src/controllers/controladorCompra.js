09e25cdec9a7ff6e01f5107f9ed5c829e3fa9f3d
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-03-15 08:45:38 -0500

    Se añade carrito de compras

--- src/controllers/controladorCompra.js
+++ src/controllers/controladorCompra.js
@@ -14,8 +14,35 @@ descripcion.textContent=infoProducto.descripcion
 let nombre=document.getElementById("nombre")
 nombre.textContent=infoProducto.nombre
 
-//let precio=document.getElementById("precio")
+let precio=document.getElementById("precio")
 precio.textContent=infoProducto.precio
+
+
+
+
+let infoCarrito=JSON.parse(localStorage.getItem("carrito"))
+let carrito
+let pildora=document.getElementById("pildora")
+if(infoCarrito!=null){
+    carrito=infoCarrito
+    pildora.textContent=carrito.length
+    
+}else{
+    carrito=[]
+}
+
+
+
+
+let botonagregarProducto=document.getElementById("btnagregarcarrito")
+
+
+botonagregarProducto.addEventListener("click",function(){
+    carrito.push(infoProducto)
+    pildora.textContent=carrito.length
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+})
+
 
 
 
@@ -26,26 +53,26 @@ let dolares=document.getElementById("dolares")
 //pintarProductos(productos)
 
 
+console.log(precio);
 dolares.addEventListener("click",function(dolares){
-
-
     
-    let precio1 = JSON.stringify(productos.precio);
-    console.log(precio1);
+    let precio = JSON.stringify(productos.precio);
+    console.log(precio);
     //localStorage.setItem("producto",JSON.stringify(datosProductoSeleccionar))
-
+    
     let datosProductoSeleccionar = {}
     //let tarjeta=evento.target.parentElement
-
+    
     let cantidad=(document.getElementById("cantidad").value)
     //let precio=(document.getElementById("precio").)
-
+    
     console.log("Se hizo clic en el botón");
     
     console.log(cantidad);
     
-   // console.log(precio);
-
+    // console.log(precio);
+    
     let resultado = cantidad * precio;
     console.log(resultado);
 });
+
